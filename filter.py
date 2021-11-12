35bc87366a63babba9b6292342acc0aa6a427002
stlad <82213506+stlad@users.noreply.github.com>
2021-11-12 20:15:30 +0500

    * Настройка параметров: размер мозаики и уровни градации

--- filter.py
+++ filter.py
@@ -4,14 +4,19 @@ import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
 
+print('Введите размер мозаики')
+size = int(input())
+print('Введите количество уровней градации ')
+step = int(255 // int(input()))
+
 i = 0
 while i < len(arr):
     j = 0
     while j < len(arr[0]):
-        s = np.mean(arr[i:i+10, j:j+10][:])
-        arr[i:i+10, j:j+10][:] = int(s // 50) * 50
-        j += 10
-    i += 10
+        s = np.mean(arr[i:i+size, j:j+size][:])
+        arr[i:i+size, j:j+size][:] = int(s // step) * step
+        j += size
+    i += size
 
 res = Image.fromarray(arr)
 res.save('res.jpg')
