1836f7e6477a64cfbf63a480bec5cfc149f451cf
stlad <82213506+stlad@users.noreply.github.com>
2021-11-12 19:28:07 +0500

    * Исправлена пролема переполнения переменной * Убраны вложенные циклы при взятии области 10*10 * Код компилируется

--- filter.py
+++ filter.py
@@ -1,5 +1,8 @@
+
+
 from PIL import Image
 import numpy as np
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
 a = len(arr)
@@ -8,20 +11,10 @@ i = 0
 while i < a - 11:
     j = 0
     while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
+        s = np.sum(arr[i:i+10, j:j+10][0:2])
         s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+
+        arr[i:i+10, j:j+10][:] = int(s // 50) * 50
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
