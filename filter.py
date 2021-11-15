4c064b558cc0f8a5fa19fdd14f8b372f23657eba
stlad <82213506+stlad@users.noreply.github.com>
2021-11-15 15:11:53 +0500

    Добавлен ввод параметров уровней градации, размера мозаики. Возможность выбора исходной картинки и сохранение результата.

--- filter.py
+++ filter.py
@@ -1,13 +1,28 @@
 from PIL import Image
 import numpy as np
+import os
 
-img = Image.open("img2.jpg")
+def input_image():
+    print('Введите название файла')
+    s = input()
+    while not os.listdir().__contains__(s):
+        print('Такого файла нет, попробуйте еще раз')
+        s = input()
+    return s
+
+def input_parametrs():
+    print('Введите размер мозаики')
+    size = int(input())
+    print('Введите количество уровней градации ')
+    step = int(255 // int(input()))
+    return size, step
+
+
+name = input_image()
+size, step = input_parametrs()
+
+img = Image.open(name)
 arr = np.array(img)
-
-print('Введите размер мозаики')
-size = int(input())
-print('Введите количество уровней градации ')
-step = int(255 // int(input()))
 
 i = 0
 while i < len(arr):
@@ -19,4 +34,5 @@ while i < len(arr):
     i += size
 
 res = Image.fromarray(arr)
-res.save('res.jpg')
+print('Введите название выходного файла (формат .jpg)')
+res.save(input()+'.jpg')
