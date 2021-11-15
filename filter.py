1cd2359cdb7920e13c8f6e2b4697eb43110d5dcd
Егор Плотников <94371922+EgorPlotnikov@users.noreply.github.com>
2021-11-15 18:21:41 +0500

    Этап 1

--- filter.py
+++ filter.py
@@ -18,11 +18,11 @@ while i < a:
                 M = n1 + n2 + n3
                 s += M
         s = int(s // 100)
-        for x in range(i, i + 10):
+        for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                arr[x][n1][0] = int(s // 50) * 50
-                arr[x][n1][1] = int(s // 50) * 50
-                arr[x][n1][2] = int(s // 50) * 50
+                arr[n][n1][0] = int(s // 50) * 50
+                arr[n][n1][1] = int(s // 50) * 50
+                arr[n][n1][2] = int(s // 50) * 50
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
