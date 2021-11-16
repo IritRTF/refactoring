7418e8fd00550885280b9b329aeeed7f94ddca9b
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2021-11-16 23:33:49 +0500

    all step complete

--- filter.py
+++ filter.py
@@ -22,12 +22,12 @@ def replacement_pixels(arr, window_height, window_width, grayscale):
             color = int(color // grayscale) * grayscale
             arr[h: h + window_height, w: w + window_width] = np.full(3, color)
 
-
-img = Image.open("img2.jpg")
+img = Image.open(input("введите название файла "))
 arr = np.array(img)
-window_height = 5
-window_width = 5
-grayscale = 10
+window_height = int(input("введите высоту мозаики или будет использовано значение по умолчанию ") or "10")
+window_width = int(input("введите ширину мозаики или будет использовано значение по умолчанию ") or "10")
+grayscale = int(input("введите градацию серого или будет использовано значение по умолчанию ") or "50")
 replacement_pixels(arr, window_height, window_width, grayscale)
 res = Image.fromarray(arr)
+print("complete")
 res.save('res.jpg')
