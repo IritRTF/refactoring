4eda702474bbabd4a647c156da0f5235f568d3d9
KirushaSpace <82442035+KirushaSpace@users.noreply.github.com>
2021-11-16 22:43:40 +0500

    2 step

--- filter.py
+++ filter.py
@@ -1,28 +1,45 @@
 from PIL import Image
 import numpy as np
+
+
+def get_average_window_color(arr, height, width, filter_frequency):
+    sum_window = 0
+    for n in range(height, height + filter_frequency):
+        for m in range(width, width + filter_frequency):
+            r = arr[n][m][0]
+            g = arr[n][m][1]
+            b = arr[n][m][2]
+            M = r // 3 + g // 3 + b // 3
+            sum_window += M
+    return int(sum_window // (filter_frequency * filter_frequency))
+
+
+def replacement_pixels(arr, height, width, filter_frequency, grayscale, color):
+    for n in range(height, height + filter_frequency):
+        for n1 in range(width, width + filter_frequency):
+            arr[n][n1][0] = int(color // grayscale) * grayscale
+            arr[n][n1][1] = int(color // grayscale) * grayscale
+            arr[n][n1][2] = int(color // grayscale) * grayscale
+
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                n1 = arr[n][m][0]
-                n2 = arr[n][m][1]
-                n3 = arr[n][m][2]
-                M = n1 // 3 + n2 // 3 + n3 // 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+height = len(arr)
+width = len(arr[1])
+filter_frequency = 10
+grayscale = 50
+window_height = 0
+while window_height < height:
+    window_width = 0
+    while window_width < width:
+        color_pixel = get_average_window_color(arr, window_height, window_width, filter_frequency)
+        replacement_pixels(arr,
+                           window_height,
+                           window_width,
+                           filter_frequency,
+                           grayscale,
+                           get_average_window_color(arr, window_height, window_width, filter_frequency))
+        window_width = window_width + filter_frequency
+    window_height = window_height + filter_frequency
 res = Image.fromarray(arr)
 res.save('res.jpg')
