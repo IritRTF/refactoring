e3f5fabf09bace1e0f0cefed0117dc945ff124db
foursid <vad.de.19955@gmail.com>
2021-11-16 11:45:20 +0500

    Этап 2

--- filter.py
+++ filter.py
@@ -1,30 +1,35 @@
 from PIL import Image
 import numpy as np
 
+
+def pixel_art_filter(mosaic_side: int, step: int):
+    img_length, img_height = len(img_arr), len(img_arr[1])
+    segment_x = 0
+    while segment_x < img_length:
+        segment_y = 0
+        while segment_y < img_height:
+            avg_brightness = find_average_brightness(segment_x, segment_y, mosaic_side)
+            pixel_coloring(avg_brightness, segment_x, segment_y, mosaic_side, step)
+            segment_y += mosaic_side
+        segment_x += mosaic_side
+
+
+def find_average_brightness(segment_x, segment_y, mosaic_side):
+    result = 0
+    for x in range(segment_x, segment_x + mosaic_side):
+        for y in range(segment_y, segment_y + mosaic_side):
+            result += sum(img_arr[x][y][range(3)] / 3)
+    return int(result) // (mosaic_side * mosaic_side)
+
+
+def pixel_coloring(avg_brightness, segment_x, segment_y, mosaic_side, grayscale):
+    for x in range(segment_x, segment_x + mosaic_side):
+        for y in range(segment_y, segment_y + mosaic_side):
+            img_arr[x][y][range(3)] = int(avg_brightness // grayscale) * grayscale
+
+
 np.seterr(over='ignore')
-arr = np.array(Image.open("img2.jpg"))
-a, a1 = len(arr), len(arr[1])
-i = 0
-
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                n1 = arr[x][y][0] / 3
-                n2 = arr[x][y][1] / 3
-                n3 = arr[x][y][2] / 3
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                arr[x][y][0] = int(s // 50) * 50
-                arr[x][y][1] = int(s // 50) * 50
-                arr[x][y][2] = int(s // 50) * 50
-        j += 10
-    i += 10
-
-res = Image.fromarray(arr)
-res.save('res.jpg')
+img_arr = np.array(Image.open("img2.jpg"))
+pixel_art_filter(int(input()), int(input()))
+filtered_img = Image.fromarray(img_arr)
+filtered_img.save('res.jpg')
