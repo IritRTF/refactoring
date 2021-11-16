ab89af0761327a2ce9dc0b46562d8a878eed9c2a
dclxxxvi <t.aleksandr888@mail.ru>
2021-11-16 16:31:34 +0500

    all tasks done

--- filter.py
+++ filter.py
@@ -15,13 +15,14 @@ def transform_to_mosaic(arr, p_w, p_h, gr):
             arr[i: i + p_h, j: j + p_w] = np.full(3, brightness)
 
 
-file_name = input("Введите имя файла который хотите преобразовать в мозаику:")
-res_name = input("Введите имя файла в который будет записан результат преобразования:")
-dimensions = [int(i) for i in input("Введите через запятую ширину и высоту одного элемента мозаики:").split(',')]
-gray_gradation = int(input("Введите число градаций серого:"))
+
+file_name = input("Введите имя файла который хотите преобразовать в мозаику: ")
+res_name = input("Введите имя файла в который будет записан результат преобразования: ")
+dimensions = [int(i) for i in input("Введите через запятую ширину и высоту одного элемента мозаики: ").split(',')]
+gray_gradation = int(input("Введите число градаций серого: "))
 img = Image.open(file_name)
 img_arr = np.array(img)
 transform_to_mosaic(img_arr, dimensions[0], dimensions[1], gray_gradation)
 res = Image.fromarray(img_arr)
-print("Преобразование завершено... Результат записан в файл \"" + res_name + "\"")
-res.save('res.jpg')
+print("Преобразование завершено... Результат записан в файл: \"" + res_name + "\"")
+res.save(res_name)
