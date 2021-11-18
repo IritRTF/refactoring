d42f425102b0ef414a64ecd5c233df4a4a6e7722
Mikhail Lanskikh <lanskikh-mikhail@mail.ru>
2021-11-18 11:16:14 +0500

    all tasks

--- filter.py
+++ filter.py
@@ -1,28 +1,12 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-    j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+
+arr = np.array(Image.open(input('Введите название исходного изображения с расширением:')))
+step, size = int(input('Введите количество шагов градации серого цвета:')), int(input('Введите размеры мозаики:'))
+rows, columns = len(arr), len(arr[0])
+for row in range(0, rows - size + 1, size):
+    for col in range(0, columns - size + 1, size):
+        mean = np.mean(arr[row:row + size, col:col + size])
+        arr[row:row + size, col:col + size] = mean // step * step
 res = Image.fromarray(arr)
-res.save('res.jpg')
+res.save(input('Введите название результата (изображения) с расширением:'))
