5380d032cd5794f65a4e5f8c9e08602e00912dbf
Ivan101002 <94450933+Ivan101002@users.noreply.github.com>
2021-11-23 17:31:16 +0500

    Update filter.py
    Метод вроде сделан

--- filter.py
+++ filter.py
@@ -1,28 +1,44 @@
 from PIL import Image
 import numpy as np
+
+def get_color (x, y, size_of_part, array):
+    shade = 0
+    for i in range(x, x + size_of_part):
+        for j in range(y, y + size_of_part):
+            red = arr[i][j][0]
+            green = arr[i][j][1]
+            blue = arr[i][j][2]
+            col_pix = (int(red) + int(green) + int(blue))
+            shade += col_pix
+            return shade
+            
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+STEPS = 50
+SIZE = 10
+width = len(arr)
+height = len(arr[1])
 i = 0
-while i <= a - 10:
+while i < width - SIZE - 1:
     j = 0
-    while j <= a1 - 10:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                red = arr[n][n1][0]
-                green = arr[n][n1][1]
-                blue = arr[n][n1][2]
-                M = (int(red) + int(green) + int(blue)) // 3
-                s += M
-        s = int(s // 100)
-        for x_axis in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+    while j < height - SIZE - 1:
+        shade = 0
+        """for x_axis in range(i, i + SIZE):
+            for y_axis in range(j, j + SIZE):
+                red = arr[x_axis][y_axis][0]
+                green = arr[x_axis][y_axis][1]
+                blue = arr[x_axis][y_axis][2]
+                col_pix = (int(red) + int(green) + int(blue))
+                shade += col_pix"""
+        shade = get_color(i, j, SIZE, arr)
+        shade = int(shade / 3 // SIZE ** 2)
+        for x_axis in range(i, i + SIZE):
+            for y_axis in range(j, j + SIZE):
+                arr[x_axis][y_axis][0] = int(s // STEPS) * STEPS
+                arr[x_axis][y_axis][1] = int(s // STEPS) * STEPS
+                arr[x_axis][y_axis][2] = int(s // STEPS) * STEPS
+        j = j + SIZE
+    i = i + SIZE
 res = Image.fromarray(arr)
 res.save('res.jpg')
