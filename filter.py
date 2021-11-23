a4dd5e7aeabc823f9bd0fceffb64504cc9931394
StepanBarkan <94455982+StepanBarkan@users.noreply.github.com>
2021-11-23 17:04:35 +0500

    2 Этоп

--- filter.py
+++ filter.py
@@ -1,28 +1,33 @@
 from PIL import Image
 import numpy as np
+
+def get_color ()
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
+STEPS = 50
+SIZE = 10
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i <= a - 10:
+while i < a - SIZE - 1:
     j = 0
-    while j <= a1 - 10:
+    while j < a1 - SIZE - 1:
         s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                cr = arr[n][n1][0]
-                cg= arr[n][n1][1]
-                cb = arr[n][n1][2]
-                M = (int(cr) + int(cg) + int(cb)) / 3
+        for x_axis in range(i, i + SIZE):
+            for y_axis in range(j, j + SIZE):
+                red = arr[x_axis][y_axis][0]
+                green = arr[x_axis][y_axis][1]
+                blue = arr[x_axis][y_axis][2]
+                M = (int(red) + int(green) + int(blue))
                 s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+        s = int(s / 3 // SIZE ** 2)
+        for x_axis in range(i, i + SIZE):
+            for y_axis in range(j, j + SIZE):
+                arr[x_axis][y_axis][0] = int(s // STEPS) * STEPS
+                arr[x_axis][y_axis][1] = int(s // STEPS) * STEPS
+                arr[x_axis][y_axis][2] = int(s // STEPS) * STEPS
+        j = j + SIZE
+    i = i + SIZE
 res = Image.fromarray(arr)
 res.save('res.jpg')
